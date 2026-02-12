45330dd492336d2a1440c805dc0cfd4b8679976c
Ken jordain <164806874+kenjordain4@users.noreply.github.com>
2026-02-11 21:34:42 -0500

    Add function to format time in 12-hour format

--- Alarm_notifier/format_time.py
+++ Alarm_notifier/format_time.py
@@ -1,1 +1,6 @@
-
+def format_input_time(hour,minute):
+    period="AM" if hour <12 else "PM"
+    hour_12=hour%12
+    if hour_12==0:
+        hour_12=12
+    return f"{hour_12:02d}:{minute:02d} {period}"
