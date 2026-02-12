07012f17319f80ac757e5cf562c284da97dbf9ac
Ken jordain <164806874+kenjordain4@users.noreply.github.com>
2026-02-11 21:35:29 -0500

    Delete format_time.py

--- format_time.py
+++
@@ -1,6 +0,0 @@
-def format_input_time(hour,minute):
-    period="AM" if hour <12 else "PM"
-    hour_12=hour%12
-    if hour_12==0:
-        hour_12=12
-    return f"{hour_12:02d}:{minute:02d} {period}"
